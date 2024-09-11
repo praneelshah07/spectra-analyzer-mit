3c84828fd70b825ee0d7702413216d98a1b9dcf9
praneelshah07 <praneelshah101@gmail.com>
2024-09-11 09:53:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.style import use
-from scipy.spatial.distance import pdist, squareform
-from fastcluster import linkage
-use('fast')
+import plotly.express as px
+from scipy.signal import find_peaks, savgol_filter
 
-# Set title
-st.title('Spectra Visualization App')
-
-# File uploader for data input
-uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
-
+st.title("Spectra Visualization App")
+st.write("Upload your chemical data in CSV format to start analyzing.")
+# File uploader
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    # Read the CSV file
+    data = pd.read_csv(uploaded_file)
