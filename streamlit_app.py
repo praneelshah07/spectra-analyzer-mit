aff4a1f576c9496514d5fdeac5c00a1aa1d94d5d
praneelshah07 <praneelshah101@gmail.com>
2024-09-11 09:44:38 -0700

    Testing file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.style import use
+from scipy.spatial.distance import pdist, squareform
+from fastcluster import linkage
+use('fast')
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Set title
+st.title('Spectra Visualization App')
+
+# File uploader for data input
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
