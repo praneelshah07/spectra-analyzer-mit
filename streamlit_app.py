8739dbdf280db26b03e3e6e77e8e1ce24e122d5f
praneelshah07 <praneelshah101@gmail.com>
2024-09-11 15:19:31 -0700

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,8 +93,14 @@ if data is not None:
     data['Raw_Spectra_Intensity'] = data['Raw_Spectra_Intensity'].apply(np.array)
     data['Normalized_Spectra_Intensity'] = data['Raw_Spectra_Intensity'].apply(lambda x: x / max(x))
 
-# Available columns in the DataFrame
+    # Available columns in the DataFrame
     available_columns = data.columns
+
+    # Strip whitespaces from column names in the DataFrame
+    data.columns = data.columns.str.strip()
+
+    # Check the column names after stripping
+    st.write("Available Columns:", data.columns)
 
     # List of columns to drop (only drop them if they exist in the DataFrame)
     columns_to_drop = ['InChI Code', 'inchikey', 'Produced by life', 'REF for life', 'Raw_Spectra_Intensity', 'Normalized_Spectra_Intensity']
