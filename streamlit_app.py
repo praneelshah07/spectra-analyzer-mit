a40ad4bed3c869682ba65f42d6392060d6083223
praneelshah07 <praneelshah101@gmail.com>
2024-09-11 10:02:26 -0700

    debugging 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,10 @@ st.title("Spectra Visualization App")
 st.write("Upload your chemical data in CSV format to start analyzing.")
 # File uploader
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
 if uploaded_file is not None:
     # Read the CSV file
     data = pd.read_csv(uploaded_file)
 
-# Preview the data to check for column names
-    st.write(df.head())
+    # Preview the data to check for column names
+    st.write(data.head())
